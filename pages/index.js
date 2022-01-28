5d1aaf19f0807fc6ad8fc737d01f0d97ce94e6e7
wagabooga <mattyhutwo@gmail.com>
2022-01-27 17:58:58 -0800

    save commit, added and working on Center component. Just added gradient background and user icon

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Sidebar from "../components/Sidebar";
+import Center from "../components/Center";
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        <Sidebar />
+        <Center />
+      </main>
+
+      <div> {/* Player */ }</div>
+    </div>
+  )
+}
