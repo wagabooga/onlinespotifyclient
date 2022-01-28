38a254b5ca79f8fa069c36e1869781ad74c3d7a9
wagabooga <mattyhutwo@gmail.com>
2022-01-27 16:49:27 -0800

    implimented useSession via next-auth/react, upon login getting proper credentials and tokens, completing Spotify's Auth flow

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,48 +6,39 @@ import {
   HeartIcon,
   RssIcon
 } from "@heroicons/react/outline"
+import { signOut, useSession } from"next-auth/react";
 
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+  console.log(session)
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
           <p>Log out</p>
         </button>
-      </div>
-      <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon  className="h-5 w-5"/>
           <p>Home</p>
         </button>
-      </div>
-      <div>
         <button className="flex items-center space-x-2 hover:text-white">
           <SearchIcon  className="h-5 w-5"/>
           <p>Search</p>
         </button>
-      </div>
-      <div>
         <button className="flex items-center space-x-2 hover:text-white">
           <LibraryIcon  className="h-5 w-5"/>
           <p>Your Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" /> {/* see JusInTime*/}
-      </div>
-      <div>
         <button className="flex items-center space-x-2 hover:text-white">
           <PlusCircleIcon  className="h-5 w-5"/>
           <p>Create Playlist</p>
         </button>
-      </div>
-      <div>
         <button className="flex items-center space-x-2 hover:text-white">
           <HeartIcon  className="h-5 w-5"/>
           <p>Liked Songs</p>
         </button>
-      </div>
-      <div>
         <button className="flex items-center space-x-2 hover:text-white">
           <RssIcon  className="h-5 w-5"/>
           <p>Your Playlists</p>
