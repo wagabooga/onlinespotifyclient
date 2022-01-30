319d29f376648711bb8394536401d5061c5a9b5b
wagabooga <mattyhutwo@gmail.com>
2022-01-29 22:04:09 -0800

    Fixed styling to match spotify client, added onClick on song to ping spotifyApi to set current playing song. Succesfully works for changing songs when already palying on another device

--- components/Center.js
+++ components/Center.js
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
@@ -38,7 +38,7 @@ function Center() {
   return (
   <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
     <header className="absolute top5 right-8">
-      <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
+      <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}>
         <img className="rounded-full w-10 h-10" src={session?.user.image} alt="" />
         <h2>{session?.user.name}</h2>
         <ChevronDownIcon className="h5 w-5" />
