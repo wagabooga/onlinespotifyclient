78c21599a9b7eaf44351a4af4c86afe5b65ddda6
wagabooga <mattyhutwo@gmail.com>
2022-01-29 23:49:49 -0800

    created own player component

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,19 @@
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil"
 import { currentTrackIdState, isPlayingState} from "../atoms/songAtom";
 import useSpotify from "./hooks/useSpotify";
 import useSongInfo from "./hooks/useSongInfo";
+import { HeartIcon, VolumeUpIcon as VolumeDownIcon } from "@heroicons/react/outline";
+import { 
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  RewindIcon,
+  VolumeUpIcon,
+  SwitchHorizontalIcon
+} from "@heroicons/react/solid"
 
 function Player() {
   const spotifyApi = useSpotify();
@@ -13,11 +23,51 @@ function Player() {
   const [volume, setVolume] = useState(50);
   
   const songInfo = useSongInfo();
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack()
+      .then((data) => {
+        console.log("Now playing:", data.body?.item)
+        setCurrentIdTrack(data.body?.item?.id)
+        spotifyApi.getMyCurrentPlaybackState()
+        .then((data) => {
+          setIsPlaying(data.body?.is_playing)
+        })
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      // fetch song info
+      fetchCurrentSong()
+      setVolume(50)
+    }
+  },[currentTrackId, spotifyApi, session])
   return (
-    <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* Left */}
-      <div>
-        <img src={songInfo?.album.images?.[0]?.url} alt="" />
+      <div className="flex items-center space-x-4">
+        <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+      {/* Center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon onClick={() => spotifyApi.skipToPrevious()} className="button"/>
+        
+        {isPlaying ? (
+          <PauseIcon classname="button w-10 h-10" />
+        ) : (
+          <PlayIcon className="button w-10 h-10" />
+        )}
+
+        <FastForwardIcon onClick={() => spotifyApi.skipToNext()} className="button" />
+        <ReplyIcon className="button" />
       </div>
     </div>
   );
